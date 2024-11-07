697366350aa1132a58aa4393069a1746d3be06d1
Moisés Cristiano <moisespastorio@gmail.com>
2024-11-07 09:36:37 -0300

    Components atualizados e lógica implementada

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,62 @@
 export const dataFake = [
   {
-    "id":"1",
+    "id": "1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "description": "Marvel anuncia um novo filme do Homem de Ferro, confira.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
-    "id":"2",
+    "id": "2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "description": "A Marvel e a Disney anunciaram uma nova série exclusiva no Disney+.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Novo filme do Pantera Negra lançado em breve",
+    "description": "O tão aguardado filme do Pantera Negra 2 está prestes a ser lançado nos cinemas.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "4",
+    "title": "Novo filme dos X-Men está em pós-produção",
+    "description": "O diretor dos X-Men confirmou que um novo filme está em fase de pós-produção.",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  },
+  {
+    "id": "5",
+    "title": "Doctor Strange 3 está em desenvolvimento",
+    "description": "A Marvel confirmou que a sequência de Doctor Strange está em andamento.",
+    "photoCover": "https://www.marvel.com/sites/default/files/styles/xlarge/public/2021/10/doctor-strange-multiverse-madness.jpg?itok=_DQDQHkF"
+  },
+  {
+    "id": "6",
+    "title": "O Retorno de Loki no Multiverso",
+    "description": "Loki terá uma nova série no Disney+ explorando o multiverso.",
+    "photoCover": "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/2/2c/Loki_Disney%2B_poster.jpg/revision/latest?cb=20210609114612"
+  },
+  {
+    "id": "7",
+    "title": "Spider-Man 4: O que esperar do novo filme",
+    "description": "O próximo filme do Homem-Aranha traz novas aventuras e desafios para Peter Parker.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/0C230E08A57F46B8A7E58C42009A9E619CFE89C7680E208AF24D8BB9327F1E5C/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "8",
+    "title": "Miss Marvel estreia na Disney+",
+    "description": "A nova série de Miss Marvel chega para expandir o universo Marvel no Disney+.",
+    "photoCover": "https://cdn1.epicstream.com/images/gallery/marvel-miss-marvel-1045x585.jpg"
+  },
+  {
+    "id": "9",
+    "title": "Guardians of the Galaxy 3: tudo o que sabemos",
+    "description": "O terceiro filme dos Guardiões da Galáxia promete muitas surpresas para os fãs.",
+    "photoCover": "https://www.superherohype.com/wp-content/uploads/2023/05/Guardians-of-the-Galaxy-Vol-3.jpg"
+  },
+  {
+    "id": "10",
+    "title": "Avengers: Secret Wars pode redefinir o MCU",
+    "description": "A Marvel está preparando um épico para redefinir o Universo Cinematográfico Marvel com Secret Wars.",
+    "photoCover": "https://www.cinemablend.com/img/gallery/avengers-secret-wars-and-other-upcoming-mcu-movies-and-shows.jpg"
   }
-]
+];
